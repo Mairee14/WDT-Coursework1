1d3958757da0f283e605769212b022a5e63e8731
Otaiki28 <bdlsmdsadiq@gmail.com>
2023-04-17 13:47:31 +0400

    test

--- script/main.js
+++ script/main.js
@@ -1,19 +1,4 @@
 
-// side bar js
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function () {
-  navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function () {
-  navLinks.style.left = "-100%";
-}
-let map = null;
-
-
-
-
 //Display countries Details
 
 let searchBtn = document.getElementById("search-btn");
---
+++ script/sidebar.js
@@ -0,0 +1,11 @@
+// side bar js
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+}
+let map = null;
